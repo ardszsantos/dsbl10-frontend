a410f115f09b717d1c163bd6d812be1213a5d422
ardszsantos <lokgamer01@gmail.com>
2024-10-23 16:07:30 -0300

    added react-router-dom with protected route for home

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthenticationPage from './pages/AuthenticationPage';
+import HomePage from './pages/HomePage';
+import ProtectedRoute from './components/ProtectedRoute.js'; // Assuming you save this in a separate file or adjust import accordingly
 
 function App() {
   return (
-    <div className="flex flex-col h-full items-center justify-center bg-gray-200 text-gray-700">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">Create React App + Tailwind CSS</h1>
-      </div>
-      <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <div className="mt-6 flex justify-center">
-        <a
-          className="uppercase hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="ml-10 uppercase hover:underline"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind
-        </a>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthenticationPage />} />
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
