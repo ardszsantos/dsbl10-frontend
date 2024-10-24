5ec5c587be9e2d9de6fe3f784c606b80d73323e2
ardszsantos <lokgamer01@gmail.com>
2024-10-24 14:14:21 -0300

    feaT: added toast messages

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';  // Import toast
 
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem('token');
+  if (!token) {
+    toast.warning('You must be logged in to access this page.');  // Warning toast
+  }
   return token ? children : <Navigate to="/" replace />;
 }
-
 
 export default ProtectedRoute;
