013ad1dfce23d6fa8db2a6e8bb1851dc7f35fba6
ardszsantos <lokgamer01@gmail.com>
2024-10-24 13:44:15 -0300

    feat: did some changes to the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,34 @@
-// src/pages/HomePage.js
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import NavBar from '../components/navbar';
+import PostRender from '../components/Postloader';  // Updated component
 
 function HomePage() {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Home Page!</h1>
-      <button
-        onClick={handleLogout}
-        className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600"
-      >
-        Logout
-      </button>
-    </div>
+    <>
+      <NavBar />
+      <div className="flex flex-col items-center min-h-screen bg-gray-300">
+        <h1 className="text-3xl font-bold mb-4 mt-20">Latest Posts:</h1>
+        <div className="posts-area bg-white shadow-md rounded-lg w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
+          <div className="container mx-auto p-4">
+            <h2 className="text-2xl font-bold mb-4">Posts</h2>
+            <table className="min-w-full bg-white border border-gray-300 rounded-lg">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b text-left">#</th>
+                  <th className="py-2 px-4 border-b text-left">Title</th>
+                  <th className="py-2 px-4 border-b text-left">Author</th>
+                  <th className="py-2 px-4 border-b text-left">Date</th>
+                  <th className="py-2 px-4 border-b text-left">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <PostRender /> {/* Injecting PostRender directly into the table */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
