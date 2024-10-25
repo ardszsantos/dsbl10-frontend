f1c8ca5bbff62943a03f8b9acfdeda2b90829366
ardszsantos <lokgamer01@gmail.com>
2024-10-25 17:16:09 -0300

    feat/ added a separate rout for each post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthenticationPage from './pages/AuthenticationPage';
 import HomePage from './pages/HomePage';
-import ProtectedRoute from './components/ProtectedRoute.js'; // Assuming you save this in a separate file or adjust import accordingly
+import ReadPostPage from './pages/ReadPostPage'; // Nova página do post
+import ProtectedRoute from './components/ProtectedRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <HomePage />
           </ProtectedRoute>
         } />
+        <Route path="/post/:id" element={<ReadPostPage />} /> {/* Nova rota */}
       </Routes>
     </Router>
     </>
