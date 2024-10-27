3e7d048420a11c02b811a2a9ddd0c320b078e234
ardszsantos <lokgamer01@gmail.com>
2024-10-27 16:45:46 -0300

    feat: added the dark mode logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-// App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthenticationPage from './pages/AuthenticationPage';
 import HomePage from './pages/HomePage';
-import ReadPostPage from './pages/ReadPostPage'; // Nova página do post
+import ReadPostPage from './pages/ReadPostPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('theme') === 'dark');
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-   <>
-    <ToastContainer/>
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthenticationPage />} />
-        <Route path="/home" element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        } />
-        <Route path="/post/:id" element={<ReadPostPage />} /> {/* Nova rota */}
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthenticationPage />} />
+          <Route path="/home" element={
+            <ProtectedRoute>
+              <HomePage toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+            </ProtectedRoute>
+          } />
+          <Route path="/post/:id" element={<ReadPostPage toggleTheme={toggleTheme} isDarkMode={isDarkMode} />} />
+        </Routes>
+      </Router>
     </>
   );
 }
