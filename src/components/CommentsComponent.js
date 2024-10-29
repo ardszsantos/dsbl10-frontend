a1b741f5729d4989a7afdf803af40f01a9476b4c
ardszsantos <lokgamer01@gmail.com>
2024-10-29 14:01:43 -0300

    styling: small fixes

--- src/components/CommentsComponent.js
+++ src/components/CommentsComponent.js
@@ -114,8 +114,8 @@ const CommentsComponent = ({ postId }) => {
   }
 
   return (
-    <div className="w-full lg:w-10/12 mx-auto mt-8">
-      <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Comments</h2>
+    <div className="w-full bg-gray-300 dark:bg-primary rounded-md p-5 lg:w-10/12 mx-auto mt-8">
+      <h2 className="text-2xl font-semibold  text-center mb-4 text-gray-900 dark:text-gray-100">Comments</h2>
 
       {error && (
         <div className="text-center py-2 text-red-500 dark:text-red-400">
@@ -124,10 +124,10 @@ const CommentsComponent = ({ postId }) => {
       )}
 
       {userId ? (
-        <form onSubmit={handleCommentSubmit} className="mb-6 flex flex-col md:flex-row items-center w-full">
+        <form onSubmit={handleCommentSubmit} className="mb-6 flex flex-row items-center w-full">
           <input
             type="text"
-            className="flex-grow bg-white p-3 border border-gray-300 rounded-t-lg md:rounded-l-lg md:rounded-tr-none focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:border-gray-600 transition-all duration-200"
+            className="flex-grow bg-white p-3 border  border-gray-300 rounded-t-lg md:rounded-l-lg md:rounded-tr-none focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:border-gray-600 transition-all duration-200"
             placeholder="Write a comment..."
             value={newComment}
             onChange={(e) => setNewComment(e.target.value)}
@@ -135,7 +135,7 @@ const CommentsComponent = ({ postId }) => {
           />
           <button
             type="submit"
-            className="w-full md:w-auto bg-blue-500 dark:bg-blue-700 text-white px-4 py-2 rounded-b-lg md:rounded-r-lg md:rounded-bl-none hover:bg-blue-600 dark:hover:bg-blue-800 flex items-center justify-center transition-all duration-200"
+            className="w-full md:w-auto bg-blue-500 dark:bg-blue-700 ml-2 text-white p-3 rounded-b-lg md:rounded-r-lg md:rounded-bl-none hover:bg-blue-600 dark:hover:bg-blue-800 flex items-center justify-center transition-all duration-200"
             disabled={isSubmitting}
           >
             {isSubmitting ? (
@@ -200,7 +200,7 @@ const CommentsComponent = ({ postId }) => {
           </div>
         ))
       ) : (
-        <p className="text-gray-600 dark:text-gray-400">
+        <p className="text-gray-600 text-center dark:text-gray-400">
           No comments yet. Be the first to comment!
         </p>
       )}
